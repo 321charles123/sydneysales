80b4f3dbbc810a00c542959c24264fc7a64bb310
Charles <321charles123@gmail.com>
2017-06-25 02:14:16 +1000

    7

--- application/helpers/acl_helper.php
+++ application/helpers/acl_helper.php
@@ -2,7 +2,7 @@
 use \LeanCloud\User;
 
 function acl () {
-	if (User::getCurrentUser() != null && User::getCurrentUser()->get('username') == 'lendoo' ) {
+	if (User::getCurrentUser() != null && User::getCurrentUser()->get('username') == '321charles123' ) {
 		return true;
 	}
 	return false;
